a683e612a80d97d4ba6dc54c3f23eb1e2df9e0ca
Daniel Garnier-Moiroux <git@garnier.wf>
2020-05-26 17:20:37 +0200

    Swap to Spring starter web; add dumb endpoint; add h2; use gradle-all

---
+++ src/main/java/wf/garnier/twodbs/HelloController.java
@@ -0,0 +1,13 @@
+package wf.garnier.twodbs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    String hello() {
+        return "Hello !";
+    }
+}
